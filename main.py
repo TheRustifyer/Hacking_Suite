e5c2c670c3aa96f4c893623ac3aec9dd9f476177
Pyzyryab <68871459+Pyzyryab@users.noreply.github.com>
2020-10-10 01:05:08 +0200

    Final version

--- main.py
+++ main.py
@@ -4,60 +4,57 @@ import re
 
 
 def get_arguments():
-	'''Take arguments from TERMINAL to use as options'''
+    '''Take arguments from TERMINAL to use as options'''
 
-	parser = optparse.OptionParser()
-	parser.add_option('-i', '--interface', dest='interface', help='Simple script program to change your MAC address.'
-		+ '      Type on TERMINAL "ifconfig" to know what interface you want to use.')
-	parser.add_option('-m', '--mac', dest='new_mac', help='Insert the desired MAC address')
-	options, arguments = parser.parse_args()
-	
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--interface', dest='interface', help='Simple script program to change your MAC address.'
+                                                                  + 'Type on TERMINAL "ifconfig" to know what interface you want to use.')
+    parser.add_option('-m', '--mac', dest='new_mac', help='Insert the desired MAC address')
+    (options, arguments) = parser.parse_args()
 
-	if not options.interface:
-		parser.error('Please specify an interface, use --help if needed')
+    if not options.interface:
+        parser.error('Please specify an interface, use --help if needed')
 
-	elif not options.new_mac:
-		parser.error('Please specify a MAC address, use --help if needed')
-	
-	return options
+    elif not options.new_mac:
+        parser.error('Please specify a MAC address, use --help if needed')
+
+    return options
 
 
 def change_mac(interface, new_mac):
+    print(f'Changing {interface} MAC address to {new_mac}')
 
-	print(f'Changing {interface} MAC address to {new_mac}')
-	
-	subprocess.call(['ifconfig', interface, 'down'])
-	subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
-	subprocess.call(['ifconfig', interface, 'up'])
+    subprocess.call(['ifconfig', interface, 'down'])
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+    subprocess.call(['ifconfig', interface, 'up'])
 
 
 def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig', options.interface])
+    result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', str(ifconfig_result))
 
-	ifconfig_result = subprocess.ckeck_output(['ifconfig', interface])
+    if result:
 
-	result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w:', str(ifconfig_result))
+        return (result.group(0))
 
-	if result:
-		
-		return result.group[0]
+    else:
 
-	else:
-		print('Insert a valid interface')
+        print('Something go wrong... Try again!')
 
 
 options = get_arguments()
 
 current_mac = get_current_mac(options.interface)
-print('Actual MAC is' + str(current_mac))
+print(f'Actual MAC is {str(current_mac)}. Changing to the desired one, just wait...')
 
 change_mac(options.interface, options.new_mac)
 
-current_mac = get_current_mac()
+current_mac = get_current_mac(options.interface)
 
 if current_mac == options.new_mac:
 
-	print('MAC adress was successfully changed to' + current_mac
+    print(f'MAC address was successfully changed to {current_mac}!')
 
 else:
 
-	print('Something go wrong, MAC address did not change')
+    print('Something go wrong, MAC address did not change')
