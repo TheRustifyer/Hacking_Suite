a80aaa2bb74d7651cc35b730a02dc3bbadcf1a14
Pyzyryab <pyzyryab@tutanota.com>
2021-01-16 12:57:13 +0100

    Created a Network Scanner

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -1,6 +1,7 @@
 # └─$ sudo python3 network_scanner.py                                                                                                                                                               1 ⨯
 
 import scapy.all as scapy
+import optparse
 
 '''
                                         ------------- INFO ----------------
@@ -23,13 +24,26 @@ Steps:
 
     -------- > Listing the INFO methods that scapy comes with
 
-    print(arp_request.summary()) # Gives us a summary about the requested object
-    print(scapy.ls(scapy.ARP)) # scapy.ls function actuates as the man command, giving you valuable info about how the object that you pass in
+    print(scapy.ARP.summary()) # Gives us a summary about the requested object
+    print(scapy.ls(scapy.ARP)) # scapy.ls function actuates as the man command, giving you valuable info about how the object that you pass in. Ex: scapy.ARP class
     [object].show() # Returns full information about the object and it's attributes
 
 '''
 
-def scan(ip):
+def get_cmd_arguments():
+
+    parser  = optparse.OptionParser()
+    parser.add_option('-t', '--target', dest='IP',
+        help='Introduce the IP range you want to scan')
+    
+    (options, arguments) = parser.parse_args()
+
+    if not options.IP:
+        options.IP = '192.168.0.1/24' # Setting broadcast as default
+    
+    return options.IP
+
+def scanner(ip):
 
     arp_request = scapy.ARP(pdst = ip) # Use ARP to ask who has the target IP
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff') # Set destination MAC to broadcast MAC
@@ -37,14 +51,33 @@ def scan(ip):
     arp_request_broadcast =  broadcast/arp_request # This is the combination of both NET packages. Scapy let us to combine packages with '/'
     # arp_request_broadcast.show() 
 
-    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1) # .srp (stands for send & receive) allows us to send packages with a custom Ether part
+    answered_list, unanswered_list = scapy.srp( # .srp (stands for send & receive) allows us to send packages with a custom Ether part
+        arp_request_broadcast, 
+        timeout=1,
+        verbose=False
+        ) 
 
-    for element in answered_list: #answered list it's a tuple. First elemnent is the package sent, second is the response
+    clients_list = []
+
+    for element in answered_list: 
+        #answered list it's a tuple. First elemnent is the package sent, second is the response
         #print(element[1].show()) #psrc stands for the IP of the package sender, and hwsrc is the hardware physical address of the client
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-    # return answered_list.summary(), answered_list.show()
+        client_dict = {'IP' : element[1].psrc, 'MAC' : element[1].hwsrc}
+        clients_list.append(client_dict)
+        
+    return clients_list
+    
+def formatted_result(result):
 
-scan('192.168.0.1/24')
+    print(f'\nThere are {len(result)} clients on this network.\n')
 
+    print('IP\t\t\tMAC Address\n------------------------------------------')
+
+    for client in result:
+        print(client['IP'] + '\t\t' + client['MAC'])
+
+
+ip = get_cmd_arguments()
+scanner_result = scanner(ip)
+formatted_result(scanner_result)
 
---
+++ README.md
@@ -0,0 +1,18 @@
+# Hacking Suite Project
+
+Hi there, and welcome to Hacking Suite. 
+
+This repository contains several projects builded as Python scripts
+that will give you the necessary tools to perform from begginer to 
+advanced hacking attacks.
+
+## Summary 
+
+1. [MAC Changer]()
+2. [Network Scanner]()
+
+## Disclaimer
+
+This scripts are built JUST as an educational purpose, in order to get a better level developing Python code while learning the common Ethical Hacking tools, allowing you to prepare for Cyber-Security IT path.
+
+You should never use this in order to make damage to anything or anyone.
